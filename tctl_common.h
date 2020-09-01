c4d0e405a0265a2a4b28a449417e38eeb2a88af9
xiangjs <951425301@qq.com>
2020-09-01 19:36:34 +0800

    增加结构体1字节对齐

--- tctl_common.h
+++ tctl_common.h
@@ -7,5 +7,6 @@
 
 #define MAX(x, y) (((x) > (y)) ? (x) : (y))
 #define MIN(x, y) (((x) < (y)) ? (x) : (y))
+#define BYTE_ALIGNED  __attribute__((packed, aligned(1)))
 
 #endif //TINY_CTL_TCTL_COMMON_H
