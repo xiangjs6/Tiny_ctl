584cd242c310b9ca310e140c02c6c82adee00d63
xiangjs <951425301@qq.com>
2020-09-01 20:50:44 +0800

    迭代器

--- iterator.h
+++ iterator.h
@@ -5,4 +5,15 @@
 #ifndef TINY_CTL_ITERATOR_H
 #define TINY_CTL_ITERATOR_H
 
+typedef struct {
+    struct {
+        void *(*increment)(void);
+        void *(*decrement)(void);
+    } *obj_func;
+    void *obj_this;
+    void *ptr;
+} iter;
+
+iter creat_iter(void *p);
+
 #endif //TINY_CTL_ITERATOR_H
