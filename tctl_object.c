f79dcccb7e81282dcfdfcd6ae77917238ad4e575
xiangjs <951425301@qq.com>
2020-09-02 15:29:27 +0800

    删除临时变量函数和对象私有结构体

--- tctl_object.c
+++ tctl_object.c
@@ -5,8 +5,6 @@
 #include "tctl_object.h"
 
 #include <pthread.h>
-#include <malloc.h>
-#include <memory.h>
 #include "allocator.h"
 
 //this指针
@@ -32,38 +30,4 @@ void **pthis(void)
         pthread_setspecific(this_key, ptr);
     }
     return ptr;
-}
-
-//临时变量
-static pthread_key_t tmp_val_key;
-static pthread_once_t tmp_val_key_once = PTHREAD_ONCE_INIT;
-struct __temp {
-    size_t size;
-    void *p;
-};
-
-static void free_temp(void *p)
-{
-    deallocate(((struct __temp*)p)->p, ((struct __temp*)p)->size);
-    deallocate(p, sizeof(struct __temp));
-}
-
-static void make_tmp_val_key(void)
-{
-    pthread_key_create(&tmp_val_key, free_temp);
-}
-
-void *tmp_val(void *p, size_t n)
-{
-    struct __temp *ptr;
-    pthread_once(&tmp_val_key_once, make_tmp_val_key);
-    if ((ptr = pthread_getspecific(tmp_val_key)) == NULL) {
-        ptr = allocate(sizeof(struct __temp));
-        ptr->size = 0;
-        ptr->p = NULL;
-    }
-    if (n > ptr->size)
-        ptr->p = reallocate(ptr->p, ptr->size, n);
-    memcpy(ptr->p, p, n);
-    return ptr->p;
 }
