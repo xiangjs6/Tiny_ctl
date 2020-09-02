c8e9cdc76feae240e55bc4f097208d6abd3e6838
xiangjs <951425301@qq.com>
2020-09-02 15:05:21 +0800

    将临时变量函数放入常用函数库

--- tctl_common.c
+++ tctl_common.c
@@ -3,3 +3,40 @@
 //
 
 #include "tctl_common.h"
+#include <pthread.h>
+#include <memory.h>
+#include "allocator.h"
+
+//临时变量
+static pthread_key_t tmp_val_key;
+static pthread_once_t tmp_val_key_once = PTHREAD_ONCE_INIT;
+struct __temp {
+    size_t size;
+    void *p;
+};
+
+static void free_temp(void *p)
+{
+    deallocate(((struct __temp*)p)->p, ((struct __temp*)p)->size);
+    deallocate(p, sizeof(struct __temp));
+}
+
+static void make_tmp_val_key(void)
+{
+    pthread_key_create(&tmp_val_key, free_temp);
+}
+
+void *tmp_val(void *p, size_t n)
+{
+    struct __temp *ptr;
+    pthread_once(&tmp_val_key_once, make_tmp_val_key);
+    if ((ptr = pthread_getspecific(tmp_val_key)) == NULL) {
+        ptr = allocate(sizeof(struct __temp));
+        ptr->size = 0;
+        ptr->p = NULL;
+    }
+    if (n > ptr->size)
+        ptr->p = reallocate(ptr->p, ptr->size, n);
+    memcpy(ptr->p, p, n);
+    return ptr->p;
+}
--- tctl_common.h
+++ tctl_common.h
@@ -4,9 +4,12 @@
 
 #ifndef TINY_CTL_TCTL_COMMON_H
 #define TINY_CTL_TCTL_COMMON_H
+#include <stddef.h>
 
 #define MAX(x, y) (((x) > (y)) ? (x) : (y))
 #define MIN(x, y) (((x) < (y)) ? (x) : (y))
 #define BYTE_ALIGNED  __attribute__((packed, aligned(1)))
 
+void *tmp_val(void *p, size_t n);
+#define TEMP(v) ({typeof(v) __temp = v; (typeof(v)*)tmp_val(&__temp, sizeof(v));})
 #endif //TINY_CTL_TCTL_COMMON_H
