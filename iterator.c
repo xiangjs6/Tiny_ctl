b859269bf8b875225a011e05572a259ab04f76c2
xiangjs <951425301@qq.com>
2020-09-04 19:48:17 +0800

    适配迭代器符合对象规则

--- iterator.c
+++ iterator.c
@@ -3,60 +3,72 @@
 //
 
 #include "iterator.h"
-#include "tctl_object.h"
 #include "allocator.h"
 #include "tctl_common.h"
 
 static void *increment(void)
 {
     iterator *p_it = *pthis();
+    __private_iterator *private_it = (__private_iterator*)p_it->__private_fill;
     void *ptr = p_it->ptr;
-    p_it->ptr = ((__iterator_obj_func*)PRIVATE_THIS(ptr))->iter_increment(p_it->ptr);
+    THIS(private_it->obj_this);
+    p_it->ptr = private_it->obj_iter_func->iter_increment(p_it->ptr);
     return ptr;
 }
 
 static void *decrement(void)
 {
     iterator *p_it = *pthis();
+    __private_iterator *private_it = (__private_iterator*)p_it->__private_fill;
     void *ptr = p_it->ptr;
-    p_it->ptr = ((__iterator_obj_func*)PRIVATE_THIS(ptr))->iter_decrement(p_it->ptr);
+    THIS(private_it->obj_this);
+    p_it->ptr = private_it->obj_iter_func->iter_decrement(p_it->ptr);
     return ptr;
 }
 
 static void *front_increment(void)
 {
     iterator *p_it = *pthis();
-    p_it->ptr = ((__iterator_obj_func*)PRIVATE_THIS(p_it->ptr))->iter_increment(p_it->ptr);
+    __private_iterator *private_it = (__private_iterator*)p_it->__private_fill;
+    THIS(private_it->obj_this);
+    p_it->ptr = private_it->obj_iter_func->iter_increment(p_it->ptr);
     return p_it->ptr;
 }
 
 static void *front_decrement(void)
 {
     iterator *p_it = *pthis();
-    p_it->ptr = ((__iterator_obj_func*)PRIVATE_THIS(p_it->ptr))->iter_decrement(p_it->ptr);
+    __private_iterator *private_it = (__private_iterator*)p_it->__private_fill;
+    THIS(private_it->obj_this);
+    p_it->ptr = private_it->obj_iter_func->iter_decrement(p_it->ptr);
     return p_it->ptr;
 }
 
 static void *add(int x)
 {
     iterator *p_it = *pthis();
-    return ((__iterator_obj_func*)PRIVATE_THIS(p_it->ptr))->iter_add(p_it->ptr, x);
+    __private_iterator *private_it = (__private_iterator*)p_it->__private_fill;
+    THIS(private_it->obj_this);
+    return private_it->obj_iter_func->iter_add(p_it->ptr, x);
 }
 
 static void *sub(int x)
 {
     iterator *p_it = *pthis();
-    return ((__iterator_obj_func*)PRIVATE_THIS(p_it->ptr))->iter_sub(p_it->ptr, x);
+    __private_iterator *private_it = (__private_iterator*)p_it->__private_fill;
+    THIS(private_it->obj_this);
+    return private_it->obj_iter_func->iter_sub(p_it->ptr, x);
 }
 
-static typeof(*((iterator*)(0))->Public_memb) def_obj_func = {increment, decrement, front_increment, front_decrement, add, sub};
+static iterator def_obj_func = {.increment = increment, .decrement = decrement, .front_increment = front_increment, .front_decrement = front_decrement, .add = add, .sub = sub};
 
-iterator init_iter(void *obj_ptr, void *p)
+iterator init_iter(void *obj_ptr, void *p, __iterator_obj_func *func)
 {
-    iterator iter;
-    iter.Public_memb = &def_obj_func;
-    iter.obj_this = obj_ptr;
+    iterator iter = def_obj_func;
     iter.ptr = p;
+    __private_iterator *p_private = (__private_iterator*)iter.__private_fill;
+    p_private->obj_this = obj_ptr;
+    p_private->obj_iter_func = func;
     return iter;
 }
 
@@ -70,10 +82,5 @@ iterator *__constructor_iter(iterator iter)
 void __destructor_iter(void *p)
 {
     iterator *iter = container_of(*(void**)p, iterator, ptr);
-    deallocate(iter, sizeof(iterator));
-}
-
-void destory_iter(void *iter)
-{
     deallocate(iter, sizeof(iterator));
 }
--- iterator.h
+++ iterator.h
@@ -4,6 +4,7 @@
 
 #ifndef TINY_CTL_ITERATOR_H
 #define TINY_CTL_ITERATOR_H
+#include "tctl_object.h"
 
 typedef struct {
     void *(*iter_increment)(void *);
@@ -13,24 +14,28 @@ typedef struct {
 } __iterator_obj_func;
 
 typedef struct {
-    struct {
-        void *(*increment)(void);
-        void *(*decrement)(void);
-        void *(*front_increment)(void);
-        void *(*front_decrement)(void);
-        void *(*add)(int);
-        void *(*sub)(int);
-    } *Public_memb;
     void *obj_this;
+    __iterator_obj_func *obj_iter_func;
+} __private_iterator;
+
+typedef struct {
     void *ptr;
+    void *(*increment)(void);
+    void *(*decrement)(void);
+    void *(*front_increment)(void);
+    void *(*front_decrement)(void);
+    void *(*add)(int);void *(*sub)(int);
+    byte __private_fill[sizeof(__private_iterator)];
 } iterator;
 
+#define __DEF_ITER_FUNC_NAME iter_func
+#define DEF_ITER_FUNC_NAME *__DEF_ITER_FUNC_NAME
 #define ITER(p) THIS(container_of((p), iterator, ptr))
 #define ITER_TYPE(type) autofree(__destructor_iter) type**
 #define ITER_VALUE(p) *p
-#define NEW_ITER(p) &(__constructor_iter(init_iter(*pthis(), p))->ptr);
-iterator init_iter(void *obj_ptr, void *p);
+#define NEW_ITER(obj, p) (&(__constructor_iter(init_iter(obj, p, (obj)->__DEF_ITER_FUNC_NAME)))->ptr);
 
+iterator init_iter(void *obj_ptr, void *p, __iterator_obj_func *func);
 iterator *__constructor_iter(iterator iter);
 void __destructor_iter(void *p);
 #endif //TINY_CTL_ITERATOR_H
