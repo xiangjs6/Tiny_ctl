4b80eefd5f0588c0e5aa70d385af11f136918a32
xiangjs <951425301@qq.com>
2020-09-04 19:46:07 +0800

    删除一些无用宏，增加对象规则声明

--- tctl_object.h
+++ tctl_object.h
@@ -6,9 +6,16 @@
 #define TINY_CTL_TCTL_OBJECT_H
 
 #include <stddef.h>
+#include "tctl_def.h"
 
+/*
+ * tctl的对象规则
+ * 1、使用对象前调用THIS或者pthis传递对象地址
+ * 2、建议将私有变量重新封装到单独结构体中，并将其大小填充到对象结构体
+ * 3、建议私有成员函数使用同一结构体指针，指向一个不变常量上
+ * 4、遵守各个泛类对象指定的规则，比如迭代器
+ * */
 void **pthis(void);
-#define THIS(p) (*((*(typeof(p))((*pthis()) = p)).Public_memb))
-#define PRIVATE_THIS(p) ((*pthis()) = p)
+#define THIS(p) ((*(typeof(p))((*pthis()) = p)))
 
 #endif //TINY_CTL_TCTL_OBJECT_H
