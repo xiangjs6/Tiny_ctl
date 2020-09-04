d1e3ca37019b02459787f83640f935eaa8618f85
xiangjs <951425301@qq.com>
2020-09-04 19:35:42 +0800

    添加tctl各个类型定义

---
+++ tctl_def.h
@@ -0,0 +1,13 @@
+//
+// Created by xjs on 2020/9/4.
+//
+
+#ifndef TINY_CTL_TCTL_DEF_H
+#define TINY_CTL_TCTL_DEF_H
+#include <stdbool.h>
+#include <stdint.h>
+#include <stddef.h>
+
+typedef unsigned char byte;
+
+#endif //TINY_CTL_TCTL_DEF_H
