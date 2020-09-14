ad701edd8b79d416a658c051f5710abc164cc53d
xiangjs <951425301@qq.com>
2020-09-14 21:43:26 +0800

    完成一部分deque接口

--- tctl_deque.c
+++ tctl_deque.c
@@ -1,5 +1,91 @@
 //
 // Created by xjs on 2020/9/12.
 //
-
+#include "tctl_allocator.h"
 #include "tctl_deque.h"
+#include <memory.h>
+
+static void extend_map(__private_deque *p_private)
+{
+    size_t old_len = p_private->mmap_len;
+    p_private->mmap_len = p_private->mmap_len ? 2 * p_private->mmap_len : 1;
+    p_private->mmap = reallocate(p_private->mmap, old_len, p_private->mmap_len);
+    memmove(p_private->mmap + sizeof(void*) * old_len / 2, p_private->mmap, old_len * sizeof(void*));
+    __deque_iter *start_iter = p_private->start.ptr;
+    __deque_iter *finish_iter = p_private->finish.ptr;
+    start_iter->map_node = p_private->mmap + sizeof(void*) * old_len / 2;
+    finish_iter->map_node = start_iter->map_node + sizeof(void*) * old_len;
+}
+iterator begin(void)
+{
+    deque *this = pop_this();
+    __private_deque *p_private = (__private_deque*)this->__obj_private;
+    return p_private->start;
+}
+iterator end(void)
+{
+    deque *this = pop_this();
+    __private_deque *p_private = (__private_deque*)this->__obj_private;
+    return p_private->finish;
+}
+size_t size(void)
+{
+    deque *this = pop_this();
+    __private_deque *p_private = (__private_deque*)this->__obj_private;
+    return p_private->nmemb;
+}
+bool empty(void)
+{
+    deque *this = pop_this();
+    __private_deque *p_private = (__private_deque*)this->__obj_private;
+    return p_private->nmemb;
+}
+void *at(int pos)
+{
+    deque *this = pop_this();
+    __private_deque *p_private = (__private_deque*)this->__obj_private;
+    if (pos >= p_private->nmemb)
+        return NULL;
+    __deque_iter *start_iter = p_private->start.ptr;
+    //__deque_iter *finish_iter = p_private->finish.ptr;
+    size_t start_len = start_iter->last - start_iter->cur;
+    //size_t finish_len = finish_iter->cur - finish_iter->first;
+    if (pos < start_len)
+        return start_iter->cur + pos * p_private->memb_size;
+    pos -= start_len;
+    int block_index = pos % (int)p_private->block_nmemb;
+    void **p_block = start_iter->map_node + block_index;
+    pos %= p_private->block_nmemb;
+    return *p_block + pos;
+}
+void const *front(void)
+{
+    deque *this = pop_this();
+    __private_deque *p_private = (__private_deque*)this->__obj_private;
+    __deque_iter *start_iter = p_private->start.ptr;
+    return start_iter->cur + p_private->memb_size;
+}
+void const *back(void)
+{
+    deque *this = pop_this();
+    __private_deque *p_private = (__private_deque*)this->__obj_private;
+    __deque_iter *finish_iter = p_private->finish.ptr;
+    return finish_iter->cur - p_private->memb_size;
+}
+void push_back(void *x)
+{
+    deque *this = pop_this();
+    __private_deque *p_private = (__private_deque*)this->__obj_private;
+    __deque_iter *finish_iter = p_private->finish.ptr;
+    if (finish_iter->cur == finish_iter->last) {
+        extend_map(p_private);
+    }
+    memcpy(finish_iter->cur, x, p_private->memb_size);
+    finish_iter->cur += p_private->memb_size;
+}
+void (*push_front)(void *x);
+void (*pop_back)(void);
+void (*pop_front)(void);
+void (*insert)(iter_ptr iter, void *x);
+void (*erase)(iter_ptr iter);
+void (*clear)(void);
--- tctl_deque.h
+++ tctl_deque.h
@@ -5,4 +5,42 @@
 #ifndef TINY_CTL_TCTL_DEQUE_H
 #define TINY_CTL_TCTL_DEQUE_H
 
+#include "tctl_object.h"
+#include "tctl_def.h"
+#include "tctl_iterator.h"
+
+typedef struct {
+    void *cur;
+    void *first;
+    void *last;
+    void **map_node;
+} __deque_iter;
+
+typedef struct {
+    size_t nmemb;
+    size_t memb_size;
+    void **mmap;
+    size_t mmap_len;
+    size_t block_nmemb;
+    iterator start;
+    iterator finish;
+} __private_deque;
+
+typedef struct {
+    iterator (*begin)(void);
+    iterator (*end)(void);
+    size_t (*size)(void);
+    bool (*empty)(void);
+    void *(*at)(int);
+    void const *(*front)(void);
+    void const *(*back)(void);
+    void (*push_back)(void *x);
+    void (*push_front)(void *x);
+    void (*pop_back)(void);
+    void (*pop_front)(void);
+    void (*insert)(iter_ptr iter, void *x);
+    void (*erase)(iter_ptr iter);
+    void (*clear)(void);
+    byte __obj_private[sizeof(__private_deque)];
+} deque;
 #endif //TINY_CTL_TCTL_DEQUE_H
