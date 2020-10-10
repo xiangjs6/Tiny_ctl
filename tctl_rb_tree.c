24af5034bbd5f1de4804f926d5d5a94408ce41bf
xiangjs <951425301@qq.com>
2020-10-10 15:54:56 +0800

    完善了头文件，完成了简单函数

--- tctl_rb_tree.c
+++ tctl_rb_tree.c
@@ -3,3 +3,85 @@
 //
 
 #include "tctl_rb_tree.h"
+#include "tctl_allocator.h"
+//private:
+struct __rb_tree_node *creat_rb_node(size_t memb_size)
+{
+    struct __rb_tree_node *node = allocate(sizeof(struct __rb_tree_node) + memb_size);
+    node->parent = node->left = node->right = NULL;
+    node->color = __rb_tree_red;
+    return node;
+}
+//public:
+__iterator const *begin(void)
+{
+    rb_tree *this = pop_this();
+    __private_rb_tree *p_private = (__private_rb_tree*)this->__obj_private;
+    p_private->start_ptr.node = p_private->header->left;
+    p_private->start_ptr.val = p_private->header->left->data;
+    return (__iterator*)&p_private->start_iter;
+}
+
+__iterator const *end(void)
+{
+    rb_tree *this = pop_this();
+    __private_rb_tree *p_private = (__private_rb_tree*)this->__obj_private;
+    p_private->finish_ptr.node = p_private->header;
+    p_private->finish_ptr.val = p_private->header->data;
+    return (__iterator*)&p_private->finish_iter;
+}
+
+bool empty(void)
+{
+    rb_tree *this = pop_this();
+    return THIS(this).size();
+}
+
+size_t size(void)
+{
+    rb_tree *this = pop_this();
+    __private_rb_tree *p_private = (__private_rb_tree*)this->__obj_private;
+    return p_private->size;
+}
+
+static __iterator_obj_func  __def_rb_tree_iter = {
+        NULL,
+        //iter_increment,
+        //iter_decrement,
+        NULL,
+        NULL,
+        NULL,
+        //iter_equal
+};
+static const iterator_func __def_rb_tree_iter_func = INIT_ITER_FUNC(&__def_rb_tree_iter);
+
+static const rb_tree _def_rb_tree = {
+
+};
+void init_rb_tree(rb_tree *p_tree, size_t memb_size, Compare cmp)
+{
+    *p_tree = _def_rb_tree;
+    __private_rb_tree *p_private = (__private_rb_tree *)p_tree->__obj_private;
+    p_private->memb_size = memb_size;
+    p_private->size = 0;
+    p_private->cmp = cmp;
+    p_private->start_iter = __creat_iter(sizeof(__rb_tree_iter), p_tree, memb_size, &__def_rb_tree_iter_func);
+    p_private->finish_iter = __creat_iter(sizeof(__rb_tree_iter), p_tree, memb_size, &__def_rb_tree_iter_func);
+    struct __rb_tree_node *node= creat_rb_node(0);
+    node->left = node->right = node->parent = node;
+    p_private->header = node;
+}
+
+void destory_rb_tree(rb_tree *p_tree)
+{
+    __private_rb_tree *p_private = (__private_rb_tree*)p_tree->__obj_private;
+    THIS(p_tree).clear();
+    deallocate(p_tree, 0);
+}
+
+rb_tree creat_rb_tree(size_t memb_size, Compare cmp)
+{
+    rb_tree tree;
+    init_rb_tree(&tree, memb_size, cmp);
+    return tree;
+}
--- tctl_rb_tree.h
+++ tctl_rb_tree.h
@@ -8,28 +8,33 @@
 #include "tctl_def.h"
 #include "tctl_iterator.h"
 
-typedef struct {
-    void *val;
-} __rb_tree_iter;
+enum __rb_tree_color {__rb_tree_red, __rb_tree_black};
+
 struct __rb_tree_node {
     struct __rb_tree_node *parent;
     struct __rb_tree_node *left;
     struct __rb_tree_node *right;
+    enum __rb_tree_color color;
     byte data[0];
 };
+
+typedef struct {
+    void *val;
+    struct __rb_tree_node *node;
+} __rb_tree_iter;
 
 typedef struct {
     size_t memb_size;
     size_t size;
     Compare cmp;
-    struct __rb_tree_node header;
+    struct __rb_tree_node *header;
     struct {
         struct __inner_iterator start_iter;
         __rb_tree_iter start_ptr;
     };
     struct {
-        struct __inner_iterator end_iter;
-        __rb_tree_iter end_ptr;
+        struct __inner_iterator finish_iter;
+        __rb_tree_iter finish_ptr;
     };
 } __private_rb_tree;
 
@@ -47,4 +52,7 @@ typedef struct {
     byte __obj_private[sizeof(__private_rb_tree)];
 } rb_tree;
 
+void init_rb_tree(rb_tree *p_tree, size_t memb_size, Compare cmp);
+void destory_rb_tree(rb_tree *p_tree);
+rb_tree creat_rb_tree(size_t memb_size, Compare cmp);
 #endif //TINY_CTL_TCTL_RB_TREE_H
