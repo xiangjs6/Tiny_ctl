c674efcd92782618fea6ff006e340220bb6ca85f
xiangjs6 <951425301@qq.com>
2020-11-15 22:52:46 +0800

    添加移植文件

---
+++ tctl_portable.c
@@ -0,0 +1,5 @@
+//
+// Created by xjs on 2020/11/15.
+//
+
+#include "tctl_portable.h"
---
+++ tctl_portable.h
@@ -0,0 +1,16 @@
+//
+// Created by xjs on 2020/11/15.
+//
+
+#ifndef TINY_CTL_TCTL_PORTABLE_H
+#define TINY_CTL_TCTL_PORTABLE_H
+#include <pthread.h>
+typedef pthread_key_t thread_key_t;
+typedef pthread_mutex_t thread_mutex_t;
+#define THREAD_MUTEX_INITIALIZER PTHREAD_MUTEX_INITIALIZER
+#define thread_getspecific pthread_getspecific
+#define thread_setspecific pthread_setspecific
+#define thread_key_create pthread_key_create
+#define thread_mutex_lock pthread_mutex_lock
+#define thread_mutex_unlock pthread_mutex_unlock
+#endif //TINY_CTL_TCTL_PORTABLE_H
