a504f920027ebbada26cff4b549dd84b5f2e6404
xiangjs6 <951425301@qq.com>
2020-11-19 00:46:27 +0800

    添加pair

--- tctl_def.h
+++ tctl_def.h
@@ -7,9 +7,12 @@
 #include <stdbool.h>
 #include <stdint.h>
 #include <stddef.h>
+#include "tctl_common.h"
 
 typedef long long offset_t;
 typedef char byte;
 
 typedef byte (*Compare)(const void *, const void *);
+
+#define pair(FT, ST) struct {FT first; ST second;} BYTE_ALIGNED
 #endif //TINY_CTL_TCTL_DEF_H
