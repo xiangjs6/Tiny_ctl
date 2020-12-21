2b25cec9c2400ac92582cbc40ae89b4f42d7222e
xiangjs6 <951425301@qq.com>
2020-12-21 04:11:02 +0000

    增加BinaryOperator

--- include/tctl_def.h
+++ include/tctl_def.h
@@ -1,4 +1,4 @@
-//
+﻿//
 // Created by xjs on 2020/9/4.
 //
 
@@ -15,6 +15,8 @@ typedef char byte;
 typedef int (*Compare)(const void *, const void *);
 typedef size_t (*HashFunc)(const void *);
 typedef void *(*ExtractKey)(const void *);
+
+typedef void *(*BinaryOperation)(const void *first, const void *second, void *res);
 
 #define pair(FT, ST) struct {FT first; ST second;} BYTE_ALIGNED
 #endif //TINY_CTL_TCTL_DEF_H
