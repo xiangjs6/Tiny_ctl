2906eaefd632f2c9bee9aa31ee9e52352c09765f
xiangjs6 <951425301@qq.com>
2021-01-24 23:12:45 +0800

    增加copy和copy_backward函数声明

--- include/tctl_algobase.h
+++ include/tctl_algobase.h
@@ -4,5 +4,7 @@
 
 #ifndef TINY_CTL_TCTL_ALGOBASE_H
 #define TINY_CTL_TCTL_ALGOBASE_H
-
+#include "tctl_iterator.h"
+Iterator copy_backward(Iterator first, Iterator last, Iterator result);
+Iterator copy(Iterator first, Iterator last, Iterator result);
 #endif //TINY_CTL_TCTL_ALGOBASE_H
