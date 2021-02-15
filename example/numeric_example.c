234729cf498b54471341572bbfe44a55d1dba518
xiangjs6 <951425301@qq.com>
2021-02-15 11:53:30 +0800

    修改例子

--- example/numeric_example.c
+++ example/numeric_example.c
@@ -1,77 +1,68 @@
-﻿#include "tctl_numeric.h"
-#include "tctl_vector.h"
-#include "auto_release_pool.h"
-#include "tctl_point_iterator.h"
+﻿#include "../include/tctl_numeric.h"
+#include "../include/tctl_vector.h"
+#include "../include/auto_release_pool.h"
+#include "../include/tctl_pointer_iterator.h"
 #include <stdio.h>
-void *binary_add(const void *first, const void *second, void *res)
+
+#define Import VECTOR, ITERATOR
+FormWO_t binary_add(FormWO_t first, FormWO_t second)
 {
-    if (!res) {
-        res = ARP_MallocARel(sizeof(int));
-        *(int*)res = 0;
-    }
-    if (!first || !second) {
-        if (first)
-            *(int*)res = *(int*)first;
-        else if (second)
-            *(int*)res = *(int*)second;
-        else
-            *(int*)res = 0;
-    } else
-        *(int*)res = *(int*)first + *(int*)second;
+    int *a = first._.f != POD ? (void*)first.mem : (void*)&first.mem;
+    int *b = second._.f != POD ? (void*)second.mem : (void*)&second.mem;
+    FormWO_t res = FORM_WITH_OBJ(T(int));
+    *(int*)&res.mem = *a + *b;
     return res;
 }
-void *binary_sub(const void *first, const void *second, void *res)
+
+FormWO_t binary_sub(FormWO_t first, FormWO_t second)
 {
-    if (!res) {
-        res = ARP_MallocARel(sizeof(int));
-        *(int*)res = 0;
-    }
-    if (!first || !second) {
-        if (first)
-            *(int*)res = *(int*)first;
-        else if (second)
-            *(int*)res = -*(int*)second;
-        else
-            *(int*)res = 0;
-    } else
-        *(int*)res = *(int*)first - *(int*)second;
+    int *a = first._.f != POD ? (void*)first.mem : (void*)&first.mem;
+    int *b = second._.f != POD ? (void*)second.mem : (void*)&second.mem;
+    FormWO_t res = FORM_WITH_OBJ(T(int));
+    *(int*)&res.mem = *a - *b;
     return res;
 }
-void *binary_mul(const void *first, const void *second, void *res)
+
+FormWO_t binary_mul(FormWO_t first, FormWO_t second)
 {
-    if (!res) {
-        res = ARP_MallocARel(sizeof(int));
-        *(int*)res = 0;
-    }
-    if (!first || !second)
-        *(int*)res = 0;
-    else
-        *(int*)res = *(int*)first * *(int*)second;
+    int *a = first._.f != POD ? (void*)first.mem : (void*)&first.mem;
+    int *b = second._.f != POD ? (void*)second.mem : (void*)&second.mem;
+    FormWO_t res = FORM_WITH_OBJ(T(int));
+    *(int*)&res.mem = *a * *b;
     return res;
 }
+
+void assign(FormWO_t left, FormWO_t right)
+{
+    int *a = left._.f != POD ? (void*)left.mem : (void*)&left.mem;
+    int *b = right._.f != POD ? (void*)right.mem : (void*)&right.mem;
+    *a = *b;
+}
+
 int main(void)
 {
     ARP_CreatePool();
-    vector v = creat_vector(sizeof(int));
+    Vector v = new(T(Vector), VA(T(int)));
     for (int i = 0; i < 10; i++)
-        THIS(&v).push_back(&i);
+        THIS(v).push_back(VA(i));
     int res = 0;
-    accumulate(THIS(&v).begin(), THIS(&v).end(), &res, binary_add);
-    printf("accumulate:%d\n", res);
+    FormWO_t r = accumulate(THIS(v).begin(), THIS(v).end(), VA(res), VA_FUNC(binary_add));
+    printf("accumulate:%d\n", *(int*)&r.mem);
     int array[10] = {1, 2, 5, 7, 10, 15, 25, 30, 31, 40};
-    adjacent_difference(POINT_ITER(array, array), POINT_ITER(array + 10, array), THIS(&v).begin(), binary_sub);
+    adjacent_difference(oriPointIter(array), oriPointIter(array, 10), THIS(v).begin(), VA_FUNC(assign), VA_FUNC(binary_sub));
     printf("adjacent_difference:");
-    for (iterator(int) it = THIS(&v).begin(); !ITER(it).equal(THIS(&v).end()); ITER(it).inc())
-        printf("%d ", *it->val);
+    for (Iterator it = THIS(v).begin(); !THIS(it).equal(VA(THIS(v).end())); THIS(it).inc())
+        printf("%d ", *(int*)THIS(it).derefer());
     putchar('\n');
-    partial_sum(THIS(&v).begin(), THIS(&v).end(), THIS(&v).begin(), binary_add);
+    partial_sum(THIS(v).begin(), THIS(v).end(), THIS(v).begin(), VA_FUNC(assign), VA_FUNC(binary_add));
     printf("partial_sum:");
-    for (iterator(int) it = THIS(&v).begin(); !ITER(it).equal(THIS(&v).end()); ITER(it).inc())
-        printf("%d ", *it->val);
+    for (Iterator it = THIS(v).begin(); !THIS(it).equal(VA(THIS(v).end())); THIS(it).inc())
+        printf("%d ", *(int*)THIS(it).derefer());
     putchar('\n');
     res = 0;
-    inner_product(THIS(&v).begin(), THIS(&v).end(), THIS(&v).begin(), &res, binary_add, binary_mul);
-    printf("inner_product:%d\n", res);
-    destory_vector(&v);
+    r = inner_product(THIS(v).begin(), THIS(v).end(), THIS(v).begin(), VA(res), VA_FUNC(binary_add), VA_FUNC(binary_mul));
+    printf("inner_product:%d\n", *(int*)&r.mem);
+    delete(v);
     ARP_FreePool();
+    return 0;
 }
