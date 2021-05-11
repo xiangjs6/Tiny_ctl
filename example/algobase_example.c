52f83f334b4833d06cd559b8a67d69c5bf120900
xiangjs6 <951425301@qq.com>
2021-05-11 11:29:29 +0800

    修复例子中

--- example/algobase_example.c
+++ example/algobase_example.c
@@ -5,35 +5,43 @@
 #include "../include/tctl_algobase.h"
 #include "../include/tctl_int.h"
 #include "../include/auto_release_pool.h"
+#include "../include/tctl_arg.h"
+#include "../include/tctl_any.h"
 
-#define Import VECTOR, ITERATOR, INT
+#define Import VECTOR, ITERATOR, INT, ANY
 
 int less(void *a, void *b)
 {
-    return toInt(a) > toInt(b);
+    Int x = classOf(a) == T(Int) ? a : THIS((MetaObject)a).cast(T(Int));
+    Int y = classOf(b) == T(Int) ? b : THIS((MetaObject)b).cast(T(Int));
+    return x->val > y->val;
 }
 
-int strCmp(FormWO_t a, FormWO_t b)
+int strCmp(void *a, void *b)
 {
-    char *s1 = *(char**)a.mem;
-    char *s2 = *(char**)b.mem;
+    Any x = a;
+    Any y = b;
+    char *s1 = *(char**)THIS(x).value();
+    char *s2 = *(char**)THIS(y).value();
     return strcmp(s1, s2);
 }
 int main(void)
 {
     ARP_CreatePool();
     //init
-    int ia[9] = {0, 1, 2, 3, 4, 5, 6, 7, 8};
-    Vector iv1 = new(T(Vector), VA(T(Int), oriPointIter(ia), oriPointIter(ia, 5)));
-    Vector iv2 = new(T(Vector), VA(T(Int), oriPointIter(ia), oriPointIter(ia, 9)));
-    for (Iterator i = THIS(iv2).begin(); !THIS(i).equal(VA(THIS(iv2).end())); THIS(i).inc()) //0 1 2 3 4 5 6 7 8
+    Int ia[9];
+    for (int i = 0; i < 9; i++)
+        ia[i] = new(T(Int), VA(i), VAEND);
+    Vector iv1 = new(T(Vector), T(Int), oriPointerIter(ia), oriPointerIter(ia, 5), VAEND);
+    Vector iv2 = new(T(Vector), T(Int), oriPointerIter(ia), oriPointerIter(ia, 9), VAEND);
+    for (Iterator i = THIS(iv2).begin(); !THIS(i).equal(THIS(iv2).end()); THIS(i).inc()) //0 1 2 3 4 5 6 7 8
         printf("%lld ", ((Int)THIS(i).derefer())->val);
     putchar('\n');
 
     //mismatch
     Iterator it;
     it = mismatch(THIS(iv1).begin(), THIS(iv1).end(), THIS(iv2).begin(), VAEND)->first;
-    if (THIS(it).equal(VA(THIS(iv1).end()))) //iv1 end
+    if (THIS(it).equal(THIS(iv1).end())) //iv1 end
         printf("iv1 end\n");
     it = mismatch(THIS(iv1).begin(), THIS(iv1).end(), THIS(iv2).begin(), VAEND)->second;
     printf("%lld\n", ((Int)THIS(it).derefer())->val); //5
@@ -41,8 +49,8 @@ int main(void)
     //equal
     //true false true
     printf("%s\n", equal(THIS(iv1).begin(), THIS(iv1).end(), THIS(iv2).begin(), VAEND) ? "true" : "false");
-    printf("%s\n", equal(THIS(iv1).begin(), THIS(iv1).end(), oriPointIter(ia, 3), VAEND) ? "true" : "false");
-    printf("%s\n", equal(THIS(iv1).begin(), THIS(iv1).end(), oriPointIter(ia, 3), VA_FUNC(less)) ? "true" : "false");
+    printf("%s\n", equal(THIS(iv1).begin(), THIS(iv1).end(), oriPointerIter(ia, 3), VAEND) ? "true" : "false");
+    printf("%s\n", equal(THIS(iv1).begin(), THIS(iv1).end(), oriPointerIter(ia, 3), VA_ANY(TEMP_VAR(void*, &less), NULL)) ? "true" : "false");
 
     //fill
     fill(THIS(iv1).begin(), THIS(iv1).end(), VA(9));
@@ -51,7 +59,7 @@ int main(void)
     putchar('\n');
 
     //fill_n
-    fill_n(THIS(iv1).begin(), 3, VA(7));
+    fill_n(THIS(iv1).begin(), VA(3), VA(7));
     for (Iterator i = THIS(iv1).begin(); !THIS(i).equal(VA(THIS(iv1).end())); THIS(i).inc()) //7 7 7 9 9
         printf("%lld ", ((Int)THIS(i).derefer())->val);
     putchar('\n');
@@ -66,15 +74,14 @@ int main(void)
     putchar('\n');
 
     //max min
-    Form_t t = THIS(ite1).type();
-    printf("max:%lld\n", toInt(max(FORM_WITH_OBJ(t, THIS(ite1).derefer()), //max:9
-                             FORM_WITH_OBJ(t, THIS(ite2).derefer()), VAEND)));
-    printf("min:%lld\n", toInt(min(FORM_WITH_OBJ(t, THIS(ite1).derefer()), //min:7
-                             FORM_WITH_OBJ(t, THIS(ite2).derefer()), VAEND)));
+    printf("max:%lld\n", ((Int)(max(THIS(ite1).derefer(), //max:9
+                             THIS(ite2).derefer(), VAEND)))->val);
+    printf("max:%lld\n", ((Int)(min(THIS(ite1).derefer(), //max:7
+                                    THIS(ite2).derefer(), VAEND)))->val);
 
     //swap
     ite2 = THIS(iv2).begin();
-    swap(FORM_WITH_OBJ(t, THIS(ite1).derefer()), FORM_WITH_OBJ(t, THIS(ite2).derefer()));
+    swap(THIS(ite1).derefer(), THIS(ite2).derefer());
     printf("ite1:%lld ite2:%lld\n", ((Int)THIS(ite1).derefer())->val, ((Int)THIS(ite2).derefer())->val); //ite1:0 ite2:9
     for (Iterator i = THIS(iv1).begin(); !THIS(i).equal(VA(THIS(iv1).end())); THIS(i).inc()) //0 7 7 7 9
         printf("%lld ", ((Int)THIS(i).derefer())->val);
@@ -84,10 +91,14 @@ int main(void)
     putchar('\n');
     
     //lexicographical_compare
-    char *stra1[] = {"Jamie", "JJHou", "Jason"};
-    char *stra2[] = {"Jamie", "JJHou", "Jerry"};
-    int res  = lexicographical_compare(oriPointIter(stra1), oriPointIter(stra1, 3),
-                                       oriPointIter(stra2), oriPointIter(stra2, 3), VA_FUNC(strCmp));
+    char *strs[4] = {"Jamie", "JJHou", "Jason", "Jerry"};
+    Any anys[4];
+    for (int i = 0; i < 4; i++)
+        anys[i] = VA_ANY(strs[i], NULL);
+    Any stra1[] = {anys[0], anys[1], anys[2]};
+    Any stra2[] = {anys[0], anys[1], anys[3]};
+    int res  = lexicographical_compare(oriPointerIter(stra1), oriPointerIter(stra1, 3),
+                                       oriPointerIter(stra2), oriPointerIter(stra2, 3), VA_ANY(TEMP_VAR(void*, strCmp), NULL));
     printf("%d\n", res); //1
     ARP_FreePool();
     return 0;
